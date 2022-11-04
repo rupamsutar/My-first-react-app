4a68a32185a6263dd1e4b387c91fa9b12fefc46b
Rupam Sutar <rupamsutar281998@gmail.com>
2022-11-04 09:33:29 +0530

    Expense Item added in components.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return  <div>
+                <div>August 20th 2022</div>
+                <div>
+                    <h2>Twinkle's Birthday</h2>
+                    <div>4000/-</div>
+                </div>                
+            </div>
+}
+
+export default ExpenseItem;
