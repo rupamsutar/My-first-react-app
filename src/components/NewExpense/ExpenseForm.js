99705e52a9653cfeed45ff81bea2054b41f454a7
Rupam Sutar <rupamsutar281998@gmail.com>
2022-11-12 10:16:05 +0530

    Form date extended

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -47,7 +47,7 @@ const ExpenseForm = (props) => {
             </div>
             <div className = "new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2022-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
+                <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
